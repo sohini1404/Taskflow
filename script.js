0a1dc18d8ee831f1528fb2fc32ff377dbf49f97e
Sohini <sohinikumar1979@gmail.com>
2026-07-07 20:52:24 +0530

    Fix ESLint warnings

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function addTask() {
   let input = document.getElementById("taskInput");
-
   let task = input.value.trim();
 
   if (task === "") {
@@ -9,10 +8,16 @@ function addTask() {
   }
 
   let li = document.createElement("li");
-
   li.innerText = task;
 
   document.getElementById("taskList").appendChild(li);
 
   input.value = "";
+
+  // Update task count
+  document.getElementById("count").innerText =
+    document.getElementById("taskList").children.length;
 }
+
+// Button click
+document.getElementById("addBtn").addEventListener("click", addTask);
