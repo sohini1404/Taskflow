84a417007edf5dcfd573f43806c7ca32c2f62673
Sohini <sohinikumar1979@gmail.com>
2026-07-07 20:06:35 +0530

    feat: initial project setup

---
+++ script.js
@@ -0,0 +1,19 @@
+function addTask(){
+
+    let input = document.getElementById("taskInput");
+
+    let task = input.value.trim();
+
+    if(task===""){
+        alert("Please enter a task");
+        return;
+    }
+
+    let li=document.createElement("li");
+
+    li.innerText=task;
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value="";
+}
